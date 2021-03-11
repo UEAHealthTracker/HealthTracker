aefb62bdfa43c7868235a7c4314095a3765efc9b
Ben Cooper <ben.f.cooper@gmail.com>
2021-03-11 12:32:08 +0000

    Added testing harness to Food class

--- src/Food.java
+++ src/Food.java
@@ -1,20 +1,30 @@
 public class Food extends Diet {
 
-    private FoodType foodType;
+    private final FoodType foodType;
+
+    public Food(FoodType foodType, String name, int calories) {
+        super();
+        this.foodType = foodType;
+        this.setName(name);
+        this.setCalorieAmount(calories);
+    }
 
     enum FoodType{
-
         VEGAN,
         PESCATERIAN,
         VEGATARIAN,
         GLUTENFREE,
         REGULAR
-
-
     }
+
+    //Testing Harness
     public static void main(String[] args) {
-        Food food = new Food();
-        food.getName();
+        Food food = new Food(FoodType.VEGAN, "Banana", 89);
+        String name = food.getName();
+        System.out.println("Name of test food: " + name);
+        System.out.println("Type of test food: " + food.foodType);
+        System.out.println("Name of test food: " + food.getCalorieAmount());
+
     }
 }
 

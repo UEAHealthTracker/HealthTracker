dcabd448271953942571c347328254c9cbe96dd9
Ben Cooper <ben.f.cooper@gmail.com>
2021-03-13 14:41:44 +0000

    Added Getters and Setters to User class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,5 @@
 
+
 public class User {
     //Initialise variables
     private String firstName;
@@ -10,15 +11,49 @@ public class User {
     private ArrayList<Group> groups = new ArrayList<Group>();
     private ArrayList<Day> days = new ArrayList<Day>();
 
+    public float getBMI() {
+        return BMI;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
     //Initialise the methods
     public static void deleteUser(User user){
         //Connect to a database and delete a user.
     }
     //Calculate the BMI of the user.
-    public float calculateBMI(){
-        float BMI = this.weight/ (height*height);
-         this.BMI= BMI;
-         return BMI;
+    public void calculateBMI(){
+        this.BMI= this.weight/ (height*height);
     }
 
     public User(String firstName, String secondName, float weight, float height) {
@@ -29,5 +64,16 @@ public class User {
         this.calculateBMI();
     }
 
+    public void addDay(Day day){
+        this.days.add(day);
+    }
+
+    public void addGoal(Goal goal){
+        this.goals.add(goal);
+    }
+
+    public void addGroup(Group group){
+        this.groups.add(group);
+    }
 
 }

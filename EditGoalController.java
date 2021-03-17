80bb148f714161d1106983268c40fa8907fb853b
Elise <78352673+eli-s-e@users.noreply.github.com>
2021-03-17 17:15:53 +0000

    Added skeleton controller files for GUI

---
+++ EditGoalController.java
@@ -0,0 +1,9 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class EditGoalController {
+
+    @FXML
+    private TextField Goalfield;
+
+}
---
+++ UserGroupPageController.java
@@ -0,0 +1,19 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class UserGroupPageController {
+
+    @FXML
+    private TextField Goalfield11;
+
+    @FXML
+    private TextField Goalfield;
+
+    @FXML
+    private TextField Goalfield1;
+
+    @FXML
+    private TextField Goalfield111;
+
+}
+

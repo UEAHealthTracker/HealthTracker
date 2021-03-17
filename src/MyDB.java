f3d869fb8c57be4baed4e65a995aa065cbfb0c88
Joshua <kingjmanisthebest@hotmail.com>
2021-03-17 15:44:13 +0000

    Trying out db connection

--- src/MyDB.java
+++ src/MyDB.java
@@ -1,3 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MyDB {
+
+    public static void main(String[] args){
+        String url = "jdbc:mysql://freedb.tech:3306/freedbtech_htrackdb?useSSL=false" ;
+       String user = "freedbtech_htrack";
+       String password = "ganttsucks123";
+
+       try{
+           Connection myConn = DriverManager.getConnection(url,user,password);
+
+           Statement myStmt = myConn.createStatement();
+
+           String sql =" UPDATE Users "+ "SET realname = David"+" WHERE realname = JaeJoon ";
+
+           myStmt.executeUpdate(sql);
+
+           System.out.println("Update Complete");
+
+       } catch (SQLException throwables) {
+           throwables.printStackTrace();
+       }
+    }
+
 
 }

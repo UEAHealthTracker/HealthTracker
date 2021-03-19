8081cb1df9088d57cf064202207e31c5d02c657c
Elise Devine <elisylveon@gmail.com>
2021-03-19 18:38:16 +0000

    update

--- src/BaseController.java
+++ src/BaseController.java
@@ -1,15 +1,37 @@
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class BaseController {
     Parent root;
     Stage stage;
     Scene scene;
+    Label userLabel;
+
+    //default constructor
+    public BaseController(){
+
+    }
+
+    //connect to current user once logged in
+    /*
+    public BaseController(User user){
+        displayUsername(User);
+    }*/
+
+    //display current logged in user's first name
+    /*
+    private void displayUsername(User user){
+        userLabel.setText(user.firstName);
+    }*/
 
     public void openHomePage(javafx.event.ActionEvent actionEvent) throws IOException {
         root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
--- src/DietPageController.java
+++ src/DietPageController.java
@@ -2,11 +2,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class DietPageController extends BaseController {
+
+    private TableView dietTable;
+
+    //add data to diet table
+    public void populateDietTable(){
+
+    }
+
+    //allow user to select a table item/row and delete it using the delete button
+    public void removeTableItem(){
+
+    }
 
     public void openAddDietItemPage(javafx.event.ActionEvent actionEvent) throws IOException {
         root = FXMLLoader.load(getClass().getResource("AddDietItemPage.fxml"));

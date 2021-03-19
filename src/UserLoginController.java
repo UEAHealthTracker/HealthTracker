98de39c3f5c545962e8b279e68783afbb6cac431
Elise Devine <elisylveon@gmail.com>
2021-03-19 16:01:35 +0000

    update

--- src/UserLoginController.java
+++ src/UserLoginController.java
@@ -14,6 +14,7 @@ public class UserLoginController {
     public void openSignUpPage(javafx.event.ActionEvent actionEvent) throws IOException {
         root = FXMLLoader.load(getClass().getResource("SignUpPage.fxml"));
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
@@ -21,13 +22,15 @@ public class UserLoginController {
     public void openLoginPage(javafx.event.ActionEvent actionEvent) throws IOException {
         root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     public void login(javafx.event.ActionEvent actionEvent) throws IOException {
-        root = FXMLLoader.load(getClass().getClassLoader().getResource("HomePage.fxml"));
+        root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }

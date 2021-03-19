ef1070bda9038268da71954981fb4bd0df77824e
Elise Devine <elisylveon@gmail.com>
2021-03-19 18:38:16 +0000

    update

--- src/WorkoutPageController.java
+++ src/WorkoutPageController.java
@@ -1,2 +1,38 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class WorkoutPageController extends BaseController {
+
+    public TableView workoutTable;
+
+    //add data to workout table
+    public void populateWorkoutTable(){
+
+    }
+
+    //allow user to select a table item/row and delete it using the delete button
+    public void removeTableItem(){
+
+    }
+
+    public void openAddWorkoutPage(javafx.event.ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("AddWorkoutPage.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void openEditWorkoutPage(javafx.event.ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("EditWorkoutPage.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }

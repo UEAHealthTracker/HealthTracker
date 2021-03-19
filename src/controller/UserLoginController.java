35b69b0bd130b24396e6af04cf08e37ff471ad88
Elise Devine <elisylveon@gmail.com>
2021-03-19 15:09:32 +0000

    update

--- src/controller/UserLoginController.java
+++ src/controller/UserLoginController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class UserLoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 }

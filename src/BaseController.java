3bdb211a61b2e7918e6d920bb00b0913fb3d036f
HMarkov <icko541@hotmail.it>
2021-03-20 13:01:45 +0000

    Update

--- src/BaseController.java
+++ src/BaseController.java
@@ -102,7 +102,7 @@ public class BaseController {
             case "AddWorkoutPage":
                 filename="AddWorkoutPage.fxml";break;
             case "EditUserPage":
-                filename="EditUserPage.fxml"; break;
+                filename="EditUserPage.fxml";
         }
         root = FXMLLoader.load(getClass().getResource(filename));
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();

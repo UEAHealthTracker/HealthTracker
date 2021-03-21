7244dede1959d846c1ee1610200abbd59a04249c
Joshua <kingjmanisthebest@hotmail.com>
2021-03-21 21:01:25 +0000

    workout class

--- src/WorkoutPageController.java
+++ src/WorkoutPageController.java
@@ -1,17 +1,29 @@
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class WorkoutPageController extends BaseController {
 
     public TableView workoutTable;
+    private static Button addWorkoutbtn;
+    private static final String SQL_Insert ="INSERT INTO workout (workoutid, sets, reps, calories, weekday) VALUES ('4','3','20','150 calories','Wednesday')";
+
 
     //add data to workout table
     public void populateWorkoutTable(){
+        Statement statement = conn.createStatement();
+        try {
+            statement.executeUpdate(SQL_Insert);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 

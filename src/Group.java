9ef4a3b15629a07b5b1ee3798890dc34ed304902
Ben Cooper <ben.f.cooper@gmail.com>
2021-04-02 17:14:20 +0100

    Added Group class and initial methods

---
+++ src/Group.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Group {
+
+    private String groupName;
+    private User groupAdmin;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public User getGroupAdmin() {
+        return groupAdmin;
+    }
+
+    public void setGroupAdmin(User groupAdmin) {
+        this.groupAdmin = groupAdmin;
+    }
+
+    public Goal getGroupGoal() {
+        return groupGoal;
+    }
+
+    public void setGroupGoal(Goal groupGoal) {
+        this.groupGoal = groupGoal;
+    }
+
+    private ArrayList<User> groupMembers;
+    private Goal groupGoal;
+    private int groupId;
+
+    public void addGroupMember(User user){
+        groupMembers.add(user);
+    }
+
+    public void deleteGroupMember(User user){
+        groupMembers.remove(user);
+    }
+
+    public void emailGroupGoal(){
+        for (User groupMember : groupMembers) {
+            String email = groupMember.email;
+            //TODO Create email class with methods
+            Email.sendGroupGoalUpdate(email);
+        }
+    }
+
+    public void deleteGroupGoal(){
+        groupGoal = null;
+    }
+}

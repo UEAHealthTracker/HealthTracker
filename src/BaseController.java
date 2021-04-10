34c4b09bd1ae8d624c9a3b97079da378d727f7e6
Joshua <kingjmanisthebest@hotmail.com>
2021-04-10 15:37:19 +0100

    Revert "Update workout"
    This reverts commit d7f9e0f8

--- src/BaseController.java
+++ src/BaseController.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import sun.awt.windows.WPrinterJob;
+//import sun.awt.windows.WPrinterJob;
 
 import java.io.IOException;
 import java.net.URL;

716a638379fb7aaf944cab62fc48efb2d4984dd7
HMarkov <icko541@hotmail.it>
2021-04-25 02:37:30 +0100

    Fix Editing User Information

--- src/BaseController.java
+++ src/BaseController.java
@@ -1,24 +1,20 @@
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import sun.awt.windows.WPrinterJob;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class BaseController {
-    public Parent root;
+    public static final BaseController Instance= new BaseController();
+    Parent root;
     Stage stage;
     Scene scene;
     Label userLabel;
-    public String fname="";
-    public final static  BaseController Instance= new BaseController();
+    public String filename="";
     //default constructor
     public BaseController(){
 
@@ -26,38 +22,46 @@ public class BaseController {
 
 
     public void MenuSwitch(javafx.event.ActionEvent actionEvent) throws IOException{
-        String text = ((Button)actionEvent.getSource()).getText();
-         String filename="";
-        switch(text){
-            case "Home":
-                filename="HomePage.fxml";break;
-            case "Diet":
-                filename="DietPage.fxml";break;
-            case "Workout":
-                filename="WorkoutPage.fxml";break;
-            case "Groups":
-                filename="GroupsPage.fxml";break;
-            case "User Information":
-                filename="UserPage.fxml";break;
-            case "Edit group":
-                filename="EditGroupPage.fxml"; break;
-            case "Create group":
-                filename="CreateGroupPage.fxml";break;
-            case "Add Diet Item":
-                filename="AddDietItemPage.fxml"; break;
-            case "EditWorkoutPage":
-                filename="EditWorkoutPage.fxml";break;
-            case "AddWorkoutPage":
-                filename="AddWorkoutPage.fxml";
-                break;
-            case "EditUserPage":
-                filename="EditUserPage.fxml";break;
+       String text = ((Button)actionEvent.getSource()).getText();
+       switch(text){
+           case "Home":
+                filename= "FXML/HomePage.fxml";break;
+           case "Diet":
+                filename= "FXML/DietPage.fxml";break;
+           case "Workout":
+                filename= "FXML/WorkoutPage.fxml";break;
+           case "Groups":
+                filename= "FXML/GroupsPage.fxml";break;
+           case "User Information":
+                filename= "FXML/UserPage.fxml";break;
+           case "Edit group":
+                filename= "FXML/EditGroupPage.fxml"; break;
+           case "Create group":
+                filename= "FXML/CreateGroupPage.fxml";break;
+           case "Add Diet Item":
+                filename= "FXML/AddDietItemPage.fxml"; break;
+           case "EditWorkoutPage":
+                filename= "FXML/EditWorkoutPage.fxml";break;
+           case "AddWorkoutPage":
+                filename= "FXML/AddWorkoutPage.fxml";break;
+           case "EditUserPage":
+                filename= "FXML/EditUserPage.fxml";break;
+            case "Join Group":
+                filename= "FXML/JoinGroup.fxml";break;
         }
-        root = FXMLLoader.load(getClass().getResource(filename));
+        root = FXMLLoader.load(getClass().getClassLoader().getResource(filename));
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        fname=filename;
+    }
+
+    public void Switch(javafx.event.ActionEvent actionEvent,String filename) throws IOException{
+        root = FXMLLoader.load(getClass().getClassLoader().getResource(filename));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
--- src/UserPageController.java
+++ src/UserPageController.java
@@ -1,35 +1,45 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.text.DecimalFormat;
 
 public class UserPageController extends BaseController{
-    @FXML
-    Label name;
+    @FXML Label name;
     @FXML Label email;
     @FXML Label height;
     @FXML Label weight;
     @FXML Label bmi;
     @FXML Label bmilabel;
     @FXML Label hellolabel;
+
+
     public void initialize() {
-        hellolabel.setText(User.INSTANCE.getUsername());
-        DecimalFormat df = new DecimalFormat("#.00");
-        double var=(User.INSTANCE.getWeight()/(User.INSTANCE.getHeight()*User.INSTANCE.getHeight()))*10000;
-        name.setText("Name: "+User.INSTANCE.getRealName());
-        email.setText("Email: "+User.INSTANCE.getEmail());
-        height.setText("Height: "+User.INSTANCE.getHeight());
-        weight.setText("Weight: "+User.INSTANCE.getWeight());
-        bmi.setText("BMI: "+df.format(var));
-        bmilabel.setUnderline(true);
-        if(var<18.5){
-            bmilabel.setText("UNDERWEIGHT");
-        }else if(var>18.5 && var<24.9){
-            bmilabel.setText("NORMAL WEIGHT");
-        }else if(var>25.0 && var<29.9){
-            bmilabel.setText("OVERWEIGHT");
-        }else{
-            bmilabel.setText("OBESE");
-        }
+
+            hellolabel.setText("Hello " + User.INSTANCE.getUsername());
+            DecimalFormat df = new DecimalFormat("#.00");
+            double var = (User.INSTANCE.getWeight() / (User.INSTANCE.getHeight() * User.INSTANCE.getHeight())) * 10000;
+            name.setText("Name: " + User.INSTANCE.getRealName());
+            email.setText("Email: " + User.INSTANCE.getEmail());
+            height.setText("Height: " + User.INSTANCE.getHeight());
+            weight.setText("Weight: " + User.INSTANCE.getWeight());
+            bmi.setText("BMI: " + df.format(var));
+            bmilabel.setUnderline(true);
+            if (var < 18.5) {
+                bmilabel.setText("UNDERWEIGHT");
+            } else if (var > 18.5 && var < 24.9) {
+                bmilabel.setText("NORMAL WEIGHT");
+            } else if (var > 25.0 && var < 29.9) {
+                bmilabel.setText("OVERWEIGHT");
+            } else {
+                bmilabel.setText("OBESE");
+            }
+//        }else if(BaseController.Instance.text=="EditUserPage.fxml"){
+//            hellolbl.setText("Hello " + User.INSTANCE.getUsername());
+//            nametf.setText(User.INSTANCE.getRealName());
+//            emailtf.setText(User.INSTANCE.getEmail());
+//            heighttf.setText(String.valueOf(User.INSTANCE.getHeight()));
+//            weighttf.setText(String.valueOf(User.INSTANCE.getWeight()));
+//        }
     }
 }
